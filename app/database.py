f6d8162c91f476629997d917c09c29336a83d6ba
Tom <tomislavignjatov@rocketmail.com>
2023-01-22 17:34:18 +0100

    add kubernetes support for mk-sentiment-fastapi

--- app/database.py
+++ app/database.py
@@ -1,17 +1,14 @@
 """Database connection."""
+import os
+
 from sqlalchemy import create_engine
-
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import sessionmaker
 
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres/macedonian_sentiment"
 
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base(engine)
